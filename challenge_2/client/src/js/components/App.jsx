7d46ef9829f7b4895cc13c43ad01a8e45295bd7c
fyun89 <fyun89@gmail.com>
2018-07-13 14:21:51 -0700

    minimum req complete

---
+++ challenge_2/client/src/js/components/App.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import BtcChart from './BtcChart';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: null };
+    this.generateDate = this.generateDate.bind(this);
+  }
+
+  componentDidMount() {
+    const currentThis = this;
+
+    axios.get('https://api.coindesk.com/v1/bpi/historical/close.json') // last 31 days' worth data
+      .then((res) => {
+        console.log(res);
+        currentThis.setState({ data: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  generateDate() {
+    this.date = new Date();
+    const month = this.date.getMonth() + 1;
+    const day = this.date.getDay();
+    const year = this.date.getFullYear();
+    const today = `${year}-${month}-${day}`;
+    return today;
+  }
+
+  render() {
+    const { data } = this.state;
+
+    return (
+      <div>
+        {
+          !data ? (
+            <div>
+            Loading
+            </div>
+          )
+            : (
+              <div>
+                <BtcChart last30Days={data} />
+              </div>
+            )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ challenge_2/client/src/js/components/BtcChart.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import PropTypes from 'prop-types';
+
+class BtcChart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleDate = this.handleDate.bind(this);
+    this.handleData = this.handleData.bind(this);
+  }
+
+  handleDate() {
+    const { last30Days } = this.props;
+    const pastDates = Object.keys(last30Days.bpi);
+    return pastDates;
+  }
+
+  handleData() {
+    const { last30Days } = this.props;
+    const dates = this.handleDate();
+    const data = [];
+    dates.forEach((elem) => {
+      data.push(last30Days.bpi[elem]);
+    });
+    return data;
+  }
+
+  render() {
+    return (
+      <div>
+        <Line data={
+          {
+            labels: this.handleDate(),
+            datasets: [{
+              label: 'Last 31 Days BTC Records',
+              data: this.handleData(),
+              backgroundColor: 'rgb(198, 253, 255, 0.5)',
+            }],
+          }
+        }
+        />
+        <a href="https://www.coindesk.com/price/">
+          Powered by CoinDesk
+        </a>
+      </div>
+    );
+  }
+}
+
+export default BtcChart;
