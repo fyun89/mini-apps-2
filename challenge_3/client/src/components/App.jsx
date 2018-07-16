5d2df8b02b8063c1c18932c55b331db3f41ac6fa
fyun89 <fyun89@gmail.com>
2018-07-15 19:18:33 -0700

    Functional button and updates scoreboard according to input

--- challenge_3/client/src/components/App.jsx
+++ challenge_3/client/src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Scores from './Scores';
+import Buttons from './Buttons';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
+      currentRound: 1,
+      turn: 1,
       rounds: [
         {
           round: 1,
@@ -58,6 +61,62 @@ class App extends Component {
         },
       ],
     };
+    this.handleClickNum = this.handleClickNum.bind(this);
+  }
+
+  handleClickNum(e) {
+    const { turn } = this.state;
+    const { currentRound } = this.state;
+    const { rounds } = this.state;
+    if (currentRound <= 9) {
+      if (turn === 1) {
+        const pastRoundInfo = rounds;
+        pastRoundInfo[currentRound - 1].first = e;
+        console.log('triggered round 1-9 turn 1')
+        this.setState({
+          rounds: pastRoundInfo,
+          turn: turn + 1,
+        });
+      } else {
+        const pastRoundInfo = rounds;
+        pastRoundInfo[currentRound - 1].second = e;
+        console.log('triggered round 1-9 turn 2')
+        this.setState({
+          rounds: pastRoundInfo,
+          turn: 1,
+          currentRound: currentRound + 1,
+        });
+      }
+    } else if (currentRound === 10) {
+      if (turn === 1) {
+        const pastRoundInfo = rounds;
+        pastRoundInfo[currentRound - 1].first = e;
+        console.log('triggered round 10 turn 1')
+        this.setState({
+          rounds: pastRoundInfo,
+          turn: turn + 1,
+        });
+      } else if (turn === 2) {
+        const pastRoundInfo = rounds;
+        pastRoundInfo[currentRound - 1].second = e;
+        console.log('triggered round 10 turn 2')
+        this.setState({
+          rounds: pastRoundInfo,
+          turn: turn + 1,
+        });
+      } else if (turn === 3) {
+        const pastRoundInfo = rounds;
+        pastRoundInfo[currentRound - 1].third = e;
+        console.log('triggered round 10 turn 3')
+        this.setState({
+          rounds: pastRoundInfo,
+          turn: 1,
+          currentRound: currentRound + 1,
+        });
+      }
+    } else if (currentRound > 10) {
+      alert('You\'ve reached the end of the game!')
+    }
   }
 
   render() {
@@ -68,6 +127,7 @@ class App extends Component {
           Bowling Scores
         </h1>
         <Scores rounds={rounds} />
+        <Buttons numButtonInput={this.handleClickNum} />
       </div>
     );
   }
--- challenge_3/client/src/components/Buttons.jsx
+++ challenge_3/client/src/components/Buttons.jsx
@@ -4,10 +4,33 @@ class Scores extends Component {
   constructor() {
     super();
     this.state = {};
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick(e) {
+    const {numButtonInput} = this.props;
+    console.log(Number(e.target.textContent));
+    numButtonInput(Number(e.target.textContent));
   }
 
   render() {
     return (
+      <div className="buttons">
+        <div className="buttonContainer">
+          <div onClick={this.handleClick} className="grid-item btn1">1</div>
+          <div onClick={this.handleClick} className="grid-item btn2">2</div>
+          <div onClick={this.handleClick} className="grid-item btn3">3</div>
+          <div onClick={this.handleClick} className="grid-item btn4">4</div>
+          <div onClick={this.handleClick} className="grid-item btn5">5</div>
+          <div onClick={this.handleClick} className="grid-item btn6">6</div>
+          <div onClick={this.handleClick} className="grid-item btn7">7</div>
+          <div onClick={this.handleClick} className="grid-item btn8">8</div>
+          <div onClick={this.handleClick} className="grid-item btn9">9</div>
+          <div onClick={this.handleClick} className="grid-item btnClr">Clr</div>
+          <div onClick={this.handleClick} className="grid-item btn10">10</div>
+          <div onClick={this.handleClick} className="grid-item btnEnt">Ent</div>
+        </div>
+      </div>
     );
   }
 }
